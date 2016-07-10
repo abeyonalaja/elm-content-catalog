6cfa93e63d29c96e281a8760346c98f1c65e3142
Abey Onalaja <abey.onalaja@gmail.com>
2016-07-10 12:47:53 +0100

    Add about route

--- src/main.elm
+++ src/main.elm
@@ -3,59 +3,56 @@ module Main exposing (..)
 import Html.App as App
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import About
+type alias Model
+  = {}
 
-type alias Model = {}
+type Msg
+  = NoOp
 
-type Msg = NoOp
-
-init : (Model, Cmd Msg)
-
-init = {} ! []
+init : ( Model, Cmd Msg )
+init =
+  {} ! []
 
 
-update : Msg -> Model -> ( Model, Cmd Msg)
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     NoOp ->
       model ! []
 
-
 subscriptions : Model -> Sub Msg
 subscriptions _ =
   Sub.none
 
-
 view : Model -> Html Msg
 view model =
   div []
-      [
-       navigationView model
-      , currentPageView model
+      [ navigationView model
+      , About.view
       ]
+
 
 navigationView : Model -> Html Msg
 navigationView model =
   nav []
-      [
-       ul []
-          [ li [] [ a [ href "#" ] [ text "Home" ]]
-          , li [] [ a [ href "#" ] [ text "Topics" ]]
-          ]
+      [ ul []
+           [ li [] [ a [ href "#" ] [ text "Home" ] ]
+           , li [] [ a [ href "#" ] [ text "Topics" ]]
+           ]
       ]
-
 
 currentPageView : Model -> Html Msg
 currentPageView model =
   text "home page"
-
-
-main : Program Never
-main =
-  App.program
-     { init = init
-     , update = update
-     , subscriptions = subscriptions
-     , view = view
-     }
       
-                 
+        
+main : Program Never
+main = App.program
+       {
+         init = init
+       , update = update
+       , subscriptions = subscriptions
+       , view = view
+       }
+
